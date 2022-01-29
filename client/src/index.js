28e3311c7b7ffaa0784c86d88c1fcff6aac0b08b
Emily Chaplais <emilychaplais@hotmail.com>
2022-01-29 21:48:16 +0000

    Correction to aws export

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Amplify, { Auth } from "aws-amplify";
-import awsconfig from "./aws-exports";
+import awsconfig from "aws-exports";
 Amplify.configure(awsconfig);
 
 ReactDOM.render(
